b32e35670b641fb61c6a66f2bad4d343886f0d48
Kaleb Shane <yepicness@gmail.com>
2017-04-03 15:12:53 -0400

    Initial

---
+++ TextDrawer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDrawer
+{
+    class Program
+    {
+        List<Picture> memory = new List<Picture>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter command or help to help");
+
+        }
+    }
+    class Picture
+    {
+        string name;
+        char[,] image;
+        public Picture(string name, int xsize, int ysize) { this.name = name; image = new char[xsize, ysize]; }
+    }
+}
